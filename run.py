52daf7e70c94f5e0ec2403dc84b303cb6e9e3a36
Felipe Ornella <felipe.ornella@swissjust.net>
2025-03-31 01:13:53 -0300

    commit renuncia JUST

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     app = create_app()
 
     # lanzar para toda la red wifi
-    # app.run(host="192.168.0.20", port=5000)
+    # app.run(host="192.168.100.20", port=5000)
     # app.run(host="192.168.0.207", port=5000)
 
     # lanzar local
